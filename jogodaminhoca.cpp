7cbb76a53520c696e7292c3f52e713201c20a2c1
Guilherme Roth Cassol <guilherme.cassol@edu.pucrs.br>
2023-08-15 11:53:19 -0300

    aprendendo a usar metodos kkj

--- jogodaminhoca.cpp
+++ jogodaminhoca.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#define TAMANHOTABULEIRO 8
+#define TAMANHOTABULEIRO 9
 
 using namespace std;
 void IniciarTabuleiro (char tabuleiro[TAMANHOTABULEIRO][TAMANHOTABULEIRO]);
@@ -9,22 +9,22 @@ class minhoca {
 	public:
 	short int tamanho;
 	short int posicao [TAMANHOTABULEIRO][TAMANHOTABULEIRO];
-
+	void MudaPosicao (short int coluna, short int linha)
+	{
+		
+	}
 };
 
 int main()
 {
 	char tabuleiro[TAMANHOTABULEIRO][TAMANHOTABULEIRO];
 	IniciarTabuleiro(tabuleiro);
+	tabuleiro [TAMANHOTABULEIRO/2][TAMANHOTABULEIRO/2] = 'O';
 	ImprimirTabuleiro(tabuleiro);
 	minhoca minhoca1;
 	minhoca1.tamanho = 1;
 	minhoca1.posicao[0][0] = 1;
-
-
-
-
-
+	
 	return 0;
 }
 
